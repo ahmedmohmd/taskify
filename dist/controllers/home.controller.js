1b72d0b98d937cd4a4c652bd826556064ce5e4cb
ahmedmohmd <er.ahmed.3301@gmail.com>
2023-09-22 03:11:21 +0300

    fix: fix tsc not found problem

--- dist/controllers/home.controller.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const home = (_, res) => {
-    res.json({ message: "Taskify Official API" });
-};
-exports.default = { home };
--- dist/utils/isValidString.util.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function isValidString(value) {
-    return !!value && value.trim() !== "";
-}
-exports.default = isValidString;
